83829f4823c1586c42302d1f3d3b8e527c9e7232
Dannyk24 <teckydan@gmail.com>
2026-07-26 16:37:07 +0100

    style search page and add remaining search filters

--- src/constants/filters.ts
+++ src/constants/filters.ts
@@ -1,9 +1,16 @@
 import { User } from "lucide-react";
+import { Briefcase } from "lucide-react";
+import { Clock } from "lucide-react";
+import { Book } from "lucide-react";
 
 export const jobTypeFilter = {
   TriggerIcon: User,
   placeholder: "Job Type",
   options: [
+    {
+      value: "",
+      label: "All",
+    },
     {
       value: "FULLTIME",
       label: "Full time",
@@ -19,6 +26,75 @@ export const jobTypeFilter = {
     {
       value: "INTERN",
       label: "Internship",
+    },
+  ],
+};
+
+export const workTypeFilter = {
+  TriggerIcon: Briefcase,
+  placeholder: "Work Type",
+  options: [
+    {
+      value: "",
+      label: "All",
+    },
+    {
+      value: "REMOTE",
+      label: "Remote",
+    },
+  ],
+};
+
+export const datePostedFilter = {
+  TriggerIcon: Clock,
+  placeholder: "Date Posted",
+  options: [
+    {
+      value: "",
+      label: "All",
+    },
+    {
+      value: "today",
+      label: "Today",
+    },
+    {
+      value: "3days",
+      label: "3 days",
+    },
+    {
+      value: "week",
+      label: "Last week",
+    },
+    {
+      value: "month",
+      label: "Last month",
+    },
+  ],
+};
+
+export const experienceLevelFilter = {
+  TriggerIcon: Book,
+  placeholder: "Experience",
+  options: [
+    {
+      value: "",
+      label: "All",
+    },
+    {
+      value: "more_than_3_years_experience",
+      label: "Over 3 years",
+    },
+    {
+      value: "under_3_years_experience",
+      label: "Under 3 years",
+    },
+    {
+      value: "no_experience",
+      label: "No experience",
+    },
+    {
+      value: "no_degree",
+      label: "No degree",
     },
   ],
 };
--- src/pages/search/SearchPage.tsx
+++ src/pages/search/SearchPage.tsx
@@ -1,10 +1,42 @@
+import { useSearchParams } from "react-router";
 import { HeroSection } from "../../components/shared/HeroSection/HeroSection";
+import * as jobFilters from "../../constants/filters";
 import "./SearchPage.css";
+import { JobCardGrid } from "../../components/shared/JobCardGrid/JobCardGrid";
+import { Select } from "../../components/ui/Select/Select";
 
 function SearchPage() {
+  const jobFiltersArray = Object.values(jobFilters);
+  const [searchParams] = useSearchParams();
+  const query = searchParams.get("query");
+
   return (
     <div className="app-page search-page">
-      <HeroSection />
+      {!query && <HeroSection />}
+      {query && (
+        <div className="search-results-shell">
+          <p className="search-results-header">
+            Search: <span>{query}</span>
+          </p>
+          <div className="search-results-actions-container">
+            <div className="search-filters-container">
+              {jobFiltersArray.map((filter, index) => {
+                return (
+                  <Select
+                    TriggerIcon={filter.TriggerIcon}
+                    placeholder={filter.placeholder}
+                    options={filter.options}
+                    key={index}
+                  />
+                );
+              })}
+            </div>
+          </div>
+          <div className="search-results-container">
+            <JobCardGrid />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
