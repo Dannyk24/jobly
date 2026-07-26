bca20b21bd9e4ae50a1b7ba330a2b6b5472d81ed
Dannyk24 <teckydan@gmail.com>
2026-07-26 17:08:49 +0100

    build card skletons for job cards

--- src/components/shared/JobCard/JobCard.tsx
+++ src/components/shared/JobCard/JobCard.tsx
@@ -1,29 +1,59 @@
 import { Dot } from "lucide-react";
 import "./JobCard.css";
+import Skeleton from "react-loading-skeleton";
 
 export function JobCard() {
+  const isLoading = false;
+
   return (
     <div className="job-card">
       <div className="job-card-left">
-        <div className="employer-logo-container">
-          <span>F</span>
-        </div>
+        {isLoading ? (
+          <Skeleton width={50} height={50} circle />
+        ) : (
+          <div className="employer-logo-container">
+            <span>F</span>
+          </div>
+        )}
         <div className="job-info-container">
-          <p className="job-title">Senior javascript developer</p>
-          <div className="employer-data">
-            <span className="employer-name">Figma</span>
-            <Dot className="decorative-dot" />
-            <span className="employer-location">san francisco ,CA</span>
-          </div>
-          <div className="job-tags">
-            <div className="data-badge">Hybrid</div>
-            <div className="data-badge">LinkedIn</div>
-          </div>
+          {isLoading ? (
+            <Skeleton width={200} height={15} />
+          ) : (
+            <p className="job-title">Senior javascript developer</p>
+          )}
+
+          {isLoading ? (
+            <Skeleton width={150} height={10} />
+          ) : (
+            <div className="employer-data">
+              <span className="employer-name">Figma</span>
+              <Dot className="decorative-dot" />
+              <span className="employer-location">san francisco ,CA</span>
+            </div>
+          )}
+
+          {isLoading ? (
+            <Skeleton width={70} height={25} borderRadius={20} />
+          ) : (
+            <div className="job-tags">
+              <div className="data-badge">Hybrid</div>
+              <div className="data-badge">LinkedIn</div>
+            </div>
+          )}
         </div>
       </div>
       <div className="job-card-right">
-        <span className="job-salary-range">$130k-$165k</span>
-        <span className="time-posted">Posted 1 day ago</span>
+        {isLoading ? (
+          <Skeleton width={80} height={20} />
+        ) : (
+          <span className="job-salary-range">$130k-$165k</span>
+        )}
+
+        {isLoading ? (
+          <Skeleton width={70} height={10} />
+        ) : (
+          <span className="time-posted">Posted 1 day ago</span>
+        )}
       </div>
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import "./index.css";
+import "react-loading-skeleton/dist/skeleton.css"; //React loading skeleton stylsheet
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
